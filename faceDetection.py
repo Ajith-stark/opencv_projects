89947bddbd74fded6aea09645ec38deb0978ed55
Ajith-stark <ajith.kum.12342@gmail.com>
2020-04-05 18:32:32 +0530

    first commit

--- faceDetection.py
+++ faceDetection.py
@@ -15,7 +15,7 @@ while True:
     for (x,y,w,h) in faces:
         cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h , x:x+w]
-        roi_color = frame[y:y+
+        roi_color = frame[y:y+h , x:x+w]
         eyes= eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
